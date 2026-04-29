25af4ba0b72f64dbb1e736cd90c9846bf423133d
Gabriel Béjar <gabriel.bejar@alumnos.uneatlantico.es>
2026-04-29 11:31:47 +0000

    fix: quitar iterativo

--- entregas/bejarGabriel/src/mergesort/MergeSortIterativo.java
+++ entregas/bejarGabriel/src/mergesort/MergeSortIterativo.java
@@ -8,17 +8,6 @@
         fusionar(array, izquierda, medio, derecha);
     }
 
-    public static void ordenarIterativo(int[] array) {
-        int n = array.length;
-        for (int tamano = 1; tamano < n; tamano *= 2) {
-            for (int izquierda = 0; izquierda < n - tamano; izquierda += 2 * tamano) {
-                int medio = izquierda + tamano - 1;
-                int derecha = Math.min(izquierda + 2 * tamano - 1, n - 1);
-                fusionar(array, izquierda, medio, derecha);
-            }
-        }
-    }
-
     private static void fusionar(int[] array, int izquierda, int medio, int derecha) {
         int tamanoIzquierda = medio - izquierda + 1;
         int tamanoDerecha = derecha - medio;
